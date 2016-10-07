1a6d04af41e72bd3570d6cc8cc68382da5316ac6
Matt Hamil <j.matthew.hamil@gmail.com>
2016-10-07 15:47:56 -0500

    Updated dotNetCore bash script

--- dotNetCoreNewWithGitIgnore.sh
+++ dotNetCoreNewWithGitIgnore.sh
@@ -1,9 +1,4 @@
-# Set an alias in .zshrc as follows:
-# alias dotnetnew=dotNetWithGitIgnore
-# Copy and paste the following function below the alias:
-function dotNetWithGitIgnore {
-  touch .gitignore
-  echo "bin\nobj\n.suo\npackages\n.DS_Store\nproject.lock.json\n.vscode" >> .gitignore
-  dotnet new
-  dotnet restore
-}
+touch .gitignore
+echo "bin\nobj\n.suo\npackages\n.DS_Store\nproject.lock.json\n.vscode" >> .gitignore
+dotnet new
+dotnet restore
