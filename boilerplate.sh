ae2f7517297ccbc7a146c01357ec1c331f9c211f
Matt Hamil <j.matthew.hamil@gmail.com>
2016-10-11 22:56:29 -0500

    Removed githubRepoMaker script from boilerplate.sh

--- boilerplate.sh
+++ boilerplate.sh
@@ -10,28 +10,3 @@ cd lib
 bower install
 npm install
 cd ../
-echo -n "Make a new repo for this project? [yes] [no]"
-read answer
-if [ "$answer" = "yes" ]
-then
-  printf "🤔  Repo Name: "
-  read name
-  myname=${name//[^[:alnum:]]/}
-  printf "$name changed in $myname for compatibilty with github\n"
-  printf "✏️  $name Description: "
-  read description
-  printf "✨  Creating repo: $myname\n"
-  # Replace occurences of "matthamil" with your Github username
-  curl -u matthamil https://api.github.com/user/repos -d '{ "name": "'"$myname"'", "description": "'"$description"'" }'
-  git init
-  touch README.md
-  echo $myname >> README.md
-  git add .
-  git commit -m "first commit"
-  git remote add origin "git@github.com:matthamil/${myname}.git"
-  git push -u origin master
-  printf "👍  Done!\n"
-else
-  echo "No new repo created. To make a new repo, run 'mkrepo'."
-fi
-
